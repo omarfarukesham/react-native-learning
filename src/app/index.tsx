67cf83a6d6e19b02f601aa8e6e53fe6a623f95e4
omar.lu86@gmail.com <omar.lu86@gmail.com>
2024-09-02 10:21:41 +0600

    feat: component and app folder is working rightly

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Greet from '../components/Greet';
+
+const Index = () => {
+  return (
+   Greet()
+  )
+}
+
+
+
+export default Index;
